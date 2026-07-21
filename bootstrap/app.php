8e2b582dc61cbaf501d86ad5d28193132693ad38
Sebastián Rodríguez <sebas@MacBook-Air-de-Sebastian.local>
2026-07-21 22:31:05 +0200

    Trust TLS reverse proxy in production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo('/admin/login');
         $middleware->validateCsrfTokens(except: ['api/display/heartbeat', 'api/display/error']);
     })
